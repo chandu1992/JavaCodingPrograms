bbee31abd4b6d47a463115e546822c9f76e355a6
chandrashekharbhaupande <chpande55@gmail.com>
2026-03-17 17:34:15 +0530

    sorting practive

--- src/main/java/com/jarvis/java8/Features/streamAPI/Sorting.java
+++ src/main/java/com/jarvis/java8/Features/streamAPI/Sorting.java
@@ -15,7 +15,6 @@ public class Sorting {
     public static void main(String[] args) {
 
         Stream<Integer> str = Stream.of(10,21,1,23,43,2,3,13);
-        Stream<Integer> str5 = Stream.of(10,21,1,23,43,2,3,13);
 
 //        Assending order small to big
         List<Integer> sortAsc =  str.sorted().collect(Collectors.toList());
@@ -54,5 +53,22 @@ public class Sorting {
         //age assending order using compareTO
         emp.stream().sorted((obj1,obj2) -> obj1.getAge().compareTo(obj2.getAge()))
                 .forEach(System.out::println);
+
+
+        List<Integer> str5 = Arrays.asList(10,21,1,23,43,2,3,13);
+
+        // 1 2 3 10 13 21 23 43
+        str5.stream().sorted((a,b)->a-b).forEach(i -> System.out.print(i+" "));
+        //43 23 21 13 10 3 2 1
+        str5.stream().sorted((a,b)->b-a).forEach(i -> System.out.print(i+" "));
+
+        //for sorted we need comparator object so if we pass as premivitve it not accept
+
+        int[] arr = new int[]{2,23,43,1,89,56,40};
+
+        Arrays.stream(arr)
+//                .boxed() // convert to Integer object we can use mapToObj() also
+                .mapToObj(value -> Integer.valueOf(value))
+                .sorted((a,b)->a-b).forEach(System.out::println);
     }
 }
